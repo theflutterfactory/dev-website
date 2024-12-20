ba100db9cb7d012d2b87eedadc35b7bfc229ff63
Julian Currie <julian.currie@gmail.com>
2024-12-21 00:27:51 +0700

    added initial hero section with some styling

---
+++ src/lib/components/HeroSection.svelte
@@ -0,0 +1,6 @@
+<section class="mx-[16vw] pt-16">
+	<h3>Hello! I'm Julian</h3>
+	<div class="mb-8"></div>
+	<h1>I'm a Senior Software Engineer with experience in several</h1>
+	<h1 class="text-[#656464]">Mobile and Web frameworks.</h1>
+</section>
