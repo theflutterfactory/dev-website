1c7bdccfad2a11e7eff8040f4fac70ec19f099be
Julian Currie <julian.currie@gmail.com>
2024-12-27 22:19:01 +0700

    Added some more Sanity types and functions to return formatted Sanity data

--- src/lib/types/sanity.d.ts
+++ src/lib/types/sanity.d.ts
@@ -16,10 +16,10 @@ type Project = {
   _createdAt: string;
   _updatedAt: string;
   _rev: string;
-  name?: string;
-  company?: string;
-  slug?: string;
-  image?: {
+  name: string;
+  company: string;
+  slug: string;
+  image: {
     asset?: {
       _ref: string;
       _type: 'reference';
@@ -30,41 +30,60 @@ type Project = {
     crop?: SanityImageCrop;
     _type: 'image';
   };
-  dateAccomplished?: string;
-  content?: Array<
-    | {
-      children?: Array<{
-        marks?: Array<string>;
-        text?: string;
-        _type: 'span';
-        _key: string;
-      }>;
-      style?: 'normal' | 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6' | 'blockquote';
-      listItem?: 'bullet' | 'number';
-      markDefs?: Array<{
-        href?: string;
-        _type: 'link';
-        _key: string;
-      }>;
-      level?: number;
-      _type: 'block';
-      _key: string;
-    }
-    | {
-      asset?: {
-        _ref: string;
-        _type: 'reference';
-        _weak?: boolean;
-        [internalGroqTypeReferenceTo]?: 'sanity.imageAsset';
-      };
-      hotspot?: SanityImageHotspot;
-      crop?: SanityImageCrop;
-      _type: 'image';
-      _key: string;
-    }
-    | ({
-      _key: string;
-    } & Slug)
-  >;
-  stack?: Array<string>;
+  dateAccomplished: string;
+  content: Array<| TextContent | ImageContent>;
+  stack: Array<string>;
+};
+
+type TextContent = {
+  children: Array<{
+    marks?: Array<string>;
+    text: string;
+    _type: 'span';
+    _key: string;
+  }>;
+  style: 'normal' | 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6' | 'blockquote';
+  listItem?: 'bullet' | 'number';
+  markDefs?: Array<{
+    href?: string;
+    _type: 'link';
+    _key: string;
+  }>;
+  level?: number;
+  _type: 'block';
+  _key: string;
+};
+
+type ImageContent = {
+  asset: {
+    _ref: string;
+    _type: 'reference';
+    _weak?: boolean;
+    [internalGroqTypeReferenceTo]?: 'sanity.imageAsset';
+  };
+  hotspot?: SanityImageHotspot;
+  crop?: SanityImageCrop;
+  _type: 'image';
+  _key: string;
+};
+
+type FormattedProject = {
+  name: string;
+  company: string;
+  dateCompleted: string;
+  stack: string[];
+  imageUrl: string;
+  slug: string;
+  content: Array<>;
+};
+
+type FormattedTextContent = {
+  type: 'text';
+  style: 'normal' | 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6' | 'blockquote';
+  textToRender: string;
+};
+
+type FormattedImageContent = {
+  type: 'image',
+  url: string;
 };
--- src/lib/utils/sanity.ts
+++ src/lib/utils/sanity.ts
@@ -1,5 +1,7 @@
 import { PUBLIC_SANITY_PROJECT_ID } from '$env/static/public';
 import { createClient, type ClientConfig } from '@sanity/client';
+import imageUrlBuilder from '@sanity/image-url';
+import type { SanityImageSource } from '@sanity/image-url/lib/types/types';
 
 const config: ClientConfig = {
   projectId: PUBLIC_SANITY_PROJECT_ID,
@@ -8,4 +10,41 @@ const config: ClientConfig = {
   apiVersion: '2024-12-26'
 };
 
-export default createClient(config);
+const sanityClient = createClient(config);
+export default sanityClient;
+
+function buildImageUrl(imageData: SanityImageSource) {
+  const imageBuilder = imageUrlBuilder(sanityClient);
+  return imageBuilder.image(imageData).url();
+}
+
+export function formatProject(project: Project) {
+  const formattedProject: FormattedProject = {
+    name: project.name,
+    company: project.company,
+    dateCompleted: project.dateAccomplished,
+    stack: project.stack,
+    slug: project.slug,
+    imageUrl: buildImageUrl(project.image),
+    content: project.content.map(formatProjectContent),
+  };
+
+  return formattedProject;
+}
+
+export function formatProjectContent(content: TextContent | ImageContent) {
+  //NOTE: Either 'block' or 'image'
+  if (content._type === 'block') {
+    const formattedTextContent: FormattedTextContent = {
+      type: 'text',
+      style: content.style,
+      textToRender: content.children.map(item => item.text).join('\n')
+    };
+    return formattedTextContent;
+  } else {
+    return {
+      type: 'image',
+      url: buildImageUrl(content)
+    };
+  }
+}
