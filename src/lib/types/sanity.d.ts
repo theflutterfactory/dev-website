618ad12a24edc6e097fc45d731181e9653e65101
Julian Currie <julian.currie@gmail.com>
2024-12-27 19:10:02 +0700

    added sanity image url pkg. added Sanity Project type

--- src/lib/types/sanity.d.ts
+++ src/lib/types/sanity.d.ts
@@ -10,3 +10,61 @@ type DevExperience = {
   endDate?: string;
 };
 
+type Project = {
+  _id: string;
+  _type: 'project';
+  _createdAt: string;
+  _updatedAt: string;
+  _rev: string;
+  name?: string;
+  company?: string;
+  slug?: string;
+  image?: {
+    asset?: {
+      _ref: string;
+      _type: 'reference';
+      _weak?: boolean;
+      [internalGroqTypeReferenceTo]?: 'sanity.imageAsset';
+    };
+    hotspot?: SanityImageHotspot;
+    crop?: SanityImageCrop;
+    _type: 'image';
+  };
+  dateAccomplished?: string;
+  content?: Array<
+    | {
+      children?: Array<{
+        marks?: Array<string>;
+        text?: string;
+        _type: 'span';
+        _key: string;
+      }>;
+      style?: 'normal' | 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6' | 'blockquote';
+      listItem?: 'bullet' | 'number';
+      markDefs?: Array<{
+        href?: string;
+        _type: 'link';
+        _key: string;
+      }>;
+      level?: number;
+      _type: 'block';
+      _key: string;
+    }
+    | {
+      asset?: {
+        _ref: string;
+        _type: 'reference';
+        _weak?: boolean;
+        [internalGroqTypeReferenceTo]?: 'sanity.imageAsset';
+      };
+      hotspot?: SanityImageHotspot;
+      crop?: SanityImageCrop;
+      _type: 'image';
+      _key: string;
+    }
+    | ({
+      _key: string;
+    } & Slug)
+  >;
+  stack?: Array<string>;
+};
