805c4000264991c50b557a4c41acb934b14d80a7
Julian Currie <julian.currie@gmail.com>
2025-01-11 17:34:06 +0700

    added tooltip for skills

--- src/lib/components/Skills.svelte
+++ src/lib/components/Skills.svelte
@@ -10,11 +10,12 @@
 
 <section class="mt-4">
 	<SectionHeader sectionName="skills">Skills</SectionHeader>
-	<div class="my-4 flex items-center justify-center">
-		<div class="grid grid-cols-5 flex-wrap justify-center gap-x-8 gap-y-8">
-			{#each skills as skill}
-				<i class={`${skill.iconClass} text-6xl `}></i>
-			{/each}
-		</div>
+	<div class="mx-auto my-10 grid grid-cols-5 place-items-center gap-x-8 gap-y-8">
+		{#each skills as skill}
+			<div class="tooltip-container">
+				<i class={`${skill.iconClass} flex text-6xl`}></i>
+				<div class="tooltip">{skill.name}</div>
+			</div>
+		{/each}
 	</div>
 </section>
